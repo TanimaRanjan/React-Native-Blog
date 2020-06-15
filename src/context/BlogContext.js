80d4f77217581c47d243005a8e7585cb1c1f080a
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-15 18:14:52 -0400

    Refactor to use useReducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 
 const BlogContext = React.createContext()
 
+const blogReducer = (state, action) => {
 
+    switch(action.type) {
+        case 'ADD_BLOG':
+            return [...state,{
+                title:`Blog Post #${state.length+1}`}];
+        case 'DELETE_BLOG':
+            return ;
+        default:
+            return state
+    }
+
+}
 export const BlogProvider = ({children}) => {
 
-    const [blogPosts, setBlogPosts ] = useState([])
+    const [blogPosts, dispatch ] = useReducer(blogReducer, [])
 
     const addBlogPost = () => {
-        setBlogPosts([...blogPosts, {
-            title:`Blog Post #${blogPosts.length+1}`}
-        ])
+        dispatch({type:'ADD_BLOG'})
     }
+
     return <BlogContext.Provider 
     value={{data:blogPosts, addBlogPost}}>
         {children}
